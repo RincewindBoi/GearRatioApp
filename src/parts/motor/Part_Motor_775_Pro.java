727b09e154e89026a68078d0ed67dfdf33dbd966
Damien - Laptop <dvfullerton@gmail.com>
2019-07-11 10:36:35 -0400

    775 info

--- src/parts/motor/Part_Motor_775_Pro.java
+++ src/parts/motor/Part_Motor_775_Pro.java
@@ -1,5 +1,16 @@
 package parts.motor;
 
 public class Part_Motor_775_Pro {
-
+	//Vendor info
+	public final String vendor       = "vex";
+	//Product info
+	public final double partnumber   = 2174347;
+	public final double price        = 19.99;
+	public final double weight       = 0.8;
+	//Product specs
+	public final double freespeed    = 18700;
+	public final double freecurrent  = 0.7;
+	public final double stallcurrent = 134;
+	public final double stalltorque  = 0.71;
+	
 }
